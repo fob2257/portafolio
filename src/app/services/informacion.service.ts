45230d9f45efebd72c85e766dcda20cf960b0b4a
fob2257 <fob2257@gmail.com>
2017-09-26 10:32:14 -0700

    Servicio de firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,14 +5,25 @@ import { Http } from '@angular/http'
 export class InformacionService {
 
   info: any = {}
+  equipo: any[] = []
   cargada: boolean = false
 
   constructor(public http: Http) {
+    this.carga_info()
+    this.carga_firebase()
+  }
+  public carga_info() {
     this.http.get('assets/data/info.pagina.json').subscribe(data => {
       // console.log(data.json())
       // this.cargada = true
       this.info = data.json()
     })
   }
-
+  public carga_firebase() {
+    this.http.get('https://paginaweb-bb5e9.firebaseio.com/equipo.json').subscribe(data => {
+      console.log(data.json())
+      this.equipo = data.json()
+    })
+  }
 }
+
