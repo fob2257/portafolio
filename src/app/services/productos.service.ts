88bd69bdcd8b7f83a6024311fd24552e5c5a7ecd
fob2257 <fob2257@gmail.com>
2017-09-28 10:33:19 -0700

    Primera version

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,10 +5,32 @@ import { Http } from '@angular/http'
 export class ProductosService {
 
   productos: any[] = []
+  productos_filtrados: any[] = []
   cargando: boolean = true
 
   constructor(private http: Http) {
     this.cargar_productos()
+  }
+
+  public buscar_producto(termino: string) {
+    if (this.productos.length === 0) {
+      this.cargar_productos().then(() => {
+        this.filtrar_productos(termino)
+      })
+    } else {
+      this.filtrar_productos(termino)
+    }
+  }
+
+  private filtrar_productos(termino: string) {
+    this.productos_filtrados = []
+    termino = termino.toLowerCase()
+    this.productos.forEach((prod, index) => {
+      if (prod.categoria.indexOf(termino) >= 0 || prod.titulo.toLowerCase().indexOf(termino) >= 0) {
+        this.productos_filtrados.push(prod)
+      }
+      // console.log(prod)
+    })
   }
 
   public cargar_producto(id: string) {
@@ -17,12 +39,16 @@ export class ProductosService {
 
   public cargar_productos() {
     this.cargando = true
-    this.http.get('https://paginaweb-bb5e9.firebaseio.com/productos_idx.json')
-      .subscribe(data => {
-        setTimeout(() => {
-          this.cargando = false
-          this.productos = data.json()
-        }, 1000)
-      })
+    let promesa = new Promise((resolve, reject) => {
+      this.http.get('https://paginaweb-bb5e9.firebaseio.com/productos_idx.json')
+        .subscribe(data => {
+          setTimeout(() => {
+            this.cargando = false
+            this.productos = data.json()
+            resolve()
+          }, 1000)
+        })
+    })
+    return promesa
   }
 }
